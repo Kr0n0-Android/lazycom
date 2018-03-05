15de62c4a0530abc4b5ced600291ed150ea50e8e
Kr0n0 <ccvals@linux.com>
2018-03-05 13:36:24 +0100

    Subida inicial proyecto Android Studio

---
+++ app/src/main/java/io/ethelhub/kr0n0/lazycom/MainActivity.java
@@ -0,0 +1,13 @@
+package io.ethelhub.kr0n0.lazycom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
